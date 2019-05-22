d9b86f7d44fed86f66511a31dd20a19eb7ec8456
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-05-22 17:11:03 +0300

    render methods

--- client/src/Inspector.js
+++ client/src/Inspector.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { CardContent, CardActions, Card, TextField } from '@material-ui/core';
@@ -8,23 +8,45 @@ const basePath = (process.env.NODE_ENV === 'production') ? '/edge' : 'http://loc
 
 const Inspector = ({ contractName, methods, onUpdateStateView }) => {
 
-  const execute = (methodName) => {
+  const [state, setState] = useState({});
+
+  const execute = (methodName, args = []) => {
+    axios.post(`${basePath}/api/execute`, {
+      type: 'tx',
+      contractName,
+      method: methodName,
+      args
+    })
   };
 
-  const renderMethod = ({ methodName, args }) => {
+  useEffect(() => {
+    const newState = methods.reduce((acc, curr) => {
+      acc[curr.methodName] = curr;
+      acc[curr.methodName].result = null;
+      return acc;
+    }, {});
+    setState(newState);
+  }, [methods]);
+
+  const renderMethod = ({ methodName, args, result }) => {
     return (
-      <Card>
+      <Card key={methodName}>
         <CardContent>
           <Typography variant="h5" component="h2">
-            {methodName} <Button size="small">Execute</Button>
+            {methodName} {' '}
+            <Button
+              onClick={() => execute(methodName)}
+              variant="contained"
+              color="secondary"
+              size="small">Execute</Button>
           </Typography>
 
-          {!!args && args.map(arg => <Typography>
-            <TextField label={arg.Name} placeholder={arg.Type} />
+          {!!args && args.map((arg, idx) => <Typography>
+            <TextField key={idx} label={arg.Name} placeholder={arg.Type} />
           </Typography>)}
         </CardContent>
         <CardActions>
-          
+          {result}
         </CardActions>
       </Card>
     )
@@ -32,7 +54,7 @@ const Inspector = ({ contractName, methods, onUpdateStateView }) => {
   return (
     <>
       <Typography variant="h6">Contract Name: {contractName}</Typography>
-      {methods.map(m => renderMethod(m))}
+      {Object.values(state).map(method => renderMethod(method))}
     </>
   );
 };
