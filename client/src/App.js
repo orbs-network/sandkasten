42e1ac6aed03656a5b63ac3a5af94964c3372436
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-05-23 08:23:45 +0300

    minor refactor

--- client/src/App.js
+++ client/src/App.js
@@ -28,119 +28,12 @@ import Grid from '@material-ui/core/Grid';
 
 import { mainListItems, secondaryListItems } from './listItems';
 import StateView from './StateView';
+import styles from './App.style';
 
 import './App.css';
 import axios from 'axios';
 import Editor from './Editor';
 import Inspector from './Inspector';
-import ContractState from './ContractState';
-
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  resultConsole: {
-    background: '#303030',
-    color: '#e8e8e8',
-    fontWeight: 400,
-    textAlign: 'left',
-    padding: 8,
-    margin: 16
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  paper: {
-    position: 'relative',
-    padding: '4px 8px',
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing.unit * 7,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing.unit * 9,
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  deployButton: {
-    position: 'absolute',
-    top: 16,
-    right: 10,
-  },
-  chartContainer: {
-    marginLeft: -22,
-  },
-  tableContainer: {
-    height: 320,
-  },
-  iconCommon: {
-    position: 'relative',
-    top: 10,
-    fontSize: 36,
-  },
-  stackMargin: {
-    marginTop: 16,
-  },
-  h5: {
-    marginBottom: theme.spacing.unit * 2,
-  },
-  dialogOverides: {
-    maxWidth: 900,
-  },
-});
 
 const basePath = (process.env.NODE_ENV === 'production') ? '/edge' : 'http://localhost:3030';
 
---
+++ client/src/App.style.js
@@ -0,0 +1,106 @@
+const drawerWidth = 240;
+
+export default theme => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  resultConsole: {
+    background: '#303030',
+    color: '#e8e8e8',
+    fontWeight: 400,
+    textAlign: 'left',
+    padding: 8,
+    margin: 16
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    position: 'relative',
+    padding: '4px 8px',
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  deployButton: {
+    position: 'absolute',
+    top: 16,
+    right: 10,
+  },
+  chartContainer: {
+    marginLeft: -22,
+  },
+  tableContainer: {
+    height: 320,
+  },
+  iconCommon: {
+    position: 'relative',
+    top: 10,
+    fontSize: 36,
+  },
+  stackMargin: {
+    marginTop: 16,
+  },
+  h5: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  dialogOverides: {
+    maxWidth: 900,
+  },
+});
